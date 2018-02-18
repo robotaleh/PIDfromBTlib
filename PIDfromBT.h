b4675dc0df6b1e2af939e522f12ccc2327fbdc86
robotaleh <alex.santos.ete@gmail.com>
2018-02-18 12:52:58 +0100

    Añade Docker a las funciones

--- PIDfromBT.h
+++ PIDfromBT.h
@@ -5,37 +5,77 @@
 class PIDfromBT
 {
 
-#define DEBUG 1
-#define NO_DEBUG 0
+#define DEBUG true
+#define NO_DEBUG false
 
-#define MIN_IDEAL 0
-#define MAX_IDEAL 500
+#define MIN_IDEAL -500  // Valor por defecto del Mínimo Ideal
+#define MAX_IDEAL 500   // Valor por defecto del Máimo Ideal
 
-#define TYPE_PIDV   1
-#define TYPE_PIDVI  2
-#define TYPE_PIDVIS 3
+#define TYPE_PIDV   1 // Tipo Proporcional, Integral, Derivada, Velocidad
+#define TYPE_PIDVI  2 // Tipo Proporcional, Integral, Derivada, Velocidad, Ideal
+#define TYPE_PIDVIS 3 // Tipo Proporcional, Integral, Derivada, Velocidad, Ideal, Succion
 
 public:
-
-  // PIDV
+  /**
+   * Constructor del objeto PIDfromBT de tipo PIDV; Proporcional, Integral, Derivada, Velocidad
+   * @param kp    Referencia a la constante de Proporcionalidad
+   * @param ki    Referencia a la constante de Integración
+   * @param kd    Referencia a la constante de Derivación
+   * @param vel   Reverencia a la variable de Velocidad
+   * @param debug Parámetro que indica si se imprimirán los valores establecidos
+   */
   PIDfromBT(float* kp, float* kd, float* ki, int* vel, bool debug);
 
-  // PIDVI
+  /**
+   * Constructor del objeto PIDfromBT de tipo PIDVI; Proporcional, Integral, Derivada, Velocidad, Ideal
+   * @param kp    Referencia a la constante de Proporcionalidad
+   * @param ki    Referencia a la constante de Integración
+   * @param kd    Referencia a la constante de Derivación
+   * @param vel   Reverencia a la variable de Velocidad
+   * @param ideal Referencia a la variable de Ideal o Consigna de Posición
+   * @param debug Parámetro que indica si se imprimirán los valores establecidos
+   */
   PIDfromBT(float* kp, float* kd, float* ki, int* vel, int* ideal, bool debug);
 
-  // PIDVIS
+
+  /**
+   * Constructor del objeto PIDfromBT de tipo PIDVIS; Proporcional, Integral, Derivada, Velocidad, Ideal, Succión
+   * @param kp      Referencia a la constante de Proporcionalidad
+   * @param ki      Referencia a la constante de Integración
+   * @param kd      Referencia a la constante de Derivación
+   * @param vel     Reverencia a la variable de Velocidad
+   * @param ideal   Referencia a la variable de Ideal o Consigna de Posición
+   * @param suction Referencia a la variable de Succion o Velocidad del Ventilador/Turbina
+   * @param debug   Parámetro que indica si se imprimirán los valores establecidos
+   */
   PIDfromBT(float* kp, float* kd, float* ki, int* vel, int* ideal, int* suction, bool debug);
 
-  void setMinIdeal(int minIdeal);
-
-  void setMaxIdeal(int maxIdeal);
-
+  /**
+   * Función para comprobar si existen comandos pendientes en el Buffer de entrada; debe ejecutarse
+   * esta función al menos una vez por cada ciclo de ejecución
+   */
   void update();
 
-  void execute_task(char letter, float value);
+  /**
+   * Función para establecer el ideal mínimo en cualquier momento
+   * @param minIdeal Valor entero para el mínimo ideal
+   */
+  void setMinIdeal(int minIdeal);
+
+  /**
+   * Función para establecer el idea máximo en cualquier momento
+   * @param maxIdeal Valor entero para el máximo ideal
+   */
+  void setMaxIdeal(int maxIdeal);
 
 private:
-  void execute_task();
+  /**
+   * Función para modificar el valor de la variable indicada por la letra
+   * @param letter Letra que identifica la variable que hay que modificar
+   * @param value  Valor que tomará la variable indicada por la Letra anterior
+   */
+  void execute_task(char letter, float value);
+
   String _value;
   char _letter;
   long _last_update;
